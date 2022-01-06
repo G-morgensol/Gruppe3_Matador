55049fff2cd5be7fe894f049fee7732554fe118d
Abbas <Abbas.k.mawla@gmail.com>
2022-01-06 18:34:03 +0100

    All Fields have been added.

--- src/main/java/Model/Board.java
+++ src/main/java/Model/Board.java
@@ -28,13 +28,35 @@ public class Board {
         fields[i++] = new Street("Valby Langgade",9,2000,new int[]{100,600,1800,5400,8000,11000},1000,"orange");
         fields[i++] = new Street("Allégade",10,2400,new int[]{150,800,2000,6000,9000,12000},1000,"orange");
         fields[i++] = new Jail("Jail",11);
-        fields[i++] = new Street("Frederiksberg Allé",12,2800,new int[]{200,1000,3000,9000,12500,15000},2000,"yellow");
+        fields[i++] = new Street("Frederiksberg Allé",12,2800,new int[]{200,1000,3000,9000,12500,15000},2000,"green");
         fields[i++] = new Brewery("Tuborg Squash",13,3000,new int[]{100,200});
-        fields[i++] = new Street("Bülowsvej",14,2800,new int[]{200,1000,3000,9000,12500,15000},2000,"yellow");
-        fields[i++] = new Street("Gl. Kongevej",15,3200,new int[]{250,1250,3750,10000,14000,18000},2000,"yellow");
+        fields[i++] = new Street("Bülowsvej",14,2800,new int[]{200,1000,3000,9000,12500,15000},2000,"green");
+        fields[i++] = new Street("Gl. Kongevej",15,3200,new int[]{250,1250,3750,10000,14000,18000},2000,"green");
         fields[i++] = new Shipping("Mols-linjen",16,4000,new int[]{500,1000,2000,4000});
-        fields[i++] = new Street("Bernstorrfsvej",17,3600,new int[]{300,1400,4000,11000,15000,19000},2000,"green");
-
+        fields[i++] = new Street("Bernstorrfsvej",17,3600,new int[]{300,1400,4000,11000,15000,19000},2000,"gray");
+        fields[i++] = new Chance("Chance",18);
+        fields[i++] = new Street("Hellerupvej",19,3600, new int[]{300,1400,4000,11000,15000,19000},2000,"gray");
+        fields[i++] = new Street("Strandvejen",20,4000, new int[]{350,1600,4400,12000,16000,20000},2000,"gray");
+        fields[i++] = new Parking("Parking",21);
+        fields[i++] = new Street("Trianglen",22,4400,new int[]{350,1800,5000,14000,17500,21000},3000,"red");
+        fields[i++] = new Chance("Chance",23);
+        fields[i++] = new Street("Østerbrogade",24,4400,new int[]{350,1800,5000,14000,17500,21000},3000,"red");
+        fields[i++] = new Street("Grønningen",25,4800,new int[]{400,2000,6000,15000,18500,22000},3000,"red");
+        fields[i++] = new Shipping("Gedser - Rostock",26,4000,new int[]{500,1000,2000,4000});
+        fields[i++] = new Street("Bredgade",27,5200,new int[]{450,2200,6600,16000,19500,23000},3000,"white");
+        fields[i++] = new Street("Kgs´-nytrov",28,5200,new int[]{450,2200,6600,16000,19500,23000},3000,"white");
+        fields[i++] = new Brewery("Coca-Cola",29,3000,new int[]{100,200});
+        fields[i++] = new Street("Østergade",30,5600,new int[]{500,2400,7200,17000,20500,24000},3000,"white");
+        fields[i++] = new Jail("De fængsles",31);
+        fields[i++] = new Street("Amagertorv",32,6000,new int[]{550,2600,7800,18000,22000,25000},4000,"yellow");
+        fields[i++] = new Street("Vimmelskaftet",33,6000,new int[]{550,2600,7800,18000,22000,25000},4000,"yellow");
+        fields[i++] = new Chance("Chance",34);
+        fields[i++] = new Street("Nygade",35,6400,new int[]{600,3000,9000,20000,24000,28000},4000,"yellow");
+        fields[i++] = new Shipping("Rødby-Puttgarden",36,4000,new int[]{500,1000,2000,4000});
+        fields[i++] = new Chance("Chance",37);
+        fields[i++] = new Street("Frederiksberggade",38,7000,new int[]{700,3500,10000,22000,26000,30000},4000,"purple");
+        fields[i++] = new Tax("Tax",39);
+        fields[i++] = new Street("Rådhuspladsen",40,8000,new int[]{1000,4000,12000,28000,34000,40000},4000,"purple");
 
 
 
