7749ae1c8a08fa8ec91e4fff40bd9d63fa83a7e6
G-morgensol <s205428@student.dtu.dk>
2022-01-06 15:03:52 +0100

    added missing method

--- src/main/java/Model/fields/Start.java
+++ src/main/java/Model/fields/Start.java
@@ -1,10 +1,14 @@
 package Model.fields;
 
+import Model.Player;
 import Model.fields.Field;
 
 public class Start extends Field {
 
     public Start(String name, int position) {
         super(name, position);
+    }
+    public void fieldAction(Player player){
+        //TODO write method here
     }
 }
