523682d43138986f80ccc133d97e05f8651bfa1e
Radyah2001 <haydar919@gmail.com>
2022-01-13 15:20:22 +0100

    ToJail done and message now shown when player lands on goToJail

--- src/main/java/Model/fields/ToJail.java
+++ src/main/java/Model/fields/ToJail.java
@@ -14,6 +14,7 @@ public class ToJail extends Field {
 
     public void fieldAction(Player player, GameView gameView){
         player.setPlayerField(jail);
-        //TODO write method here
+        player.setJailed(true);
+        gameView.gui.showMessage("You got jailed, sucks to be you");
     }
 }
